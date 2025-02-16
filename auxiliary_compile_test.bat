c5e1850c6c0ed56255045c8370e247a9581d0f59
František Milt <frantisek.milt@gmail.com>
2025-02-16 04:08:53 +0100

    Reworked scripts for projects building

--- auxiliary_compile_test.bat
+++ auxiliary_compile_test.bat
@@ -1,13 +1,20 @@
 @ECHO OFF
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
-REM setup base path for further scripts
-SET "path_base=%~dp0"
+REM inform called scripts that they are called
+SET /A mode_called=1
+
+REM directory path where this script resides
+SET "path_this=%~dp0"
+
+REM setup start and base paths
+SET "path_start=%path_this%"
+SET "path_base=%path_this%"   
 
 REM compiling an auxiliary program
-SET /A aux_build=1
+SET /A mode_aux_build=1
 
 REM run the compilation test script
-CALL "%path_base%utils\program_compile_test.bat"
+CALL "%path_this%utils\program_compile_test.bat"
 
 ENDLOCAL
--- utils/program_compile_test.bat
+++ utils/program_compile_test.bat
@@ -1,88 +1,96 @@
 @ECHO OFF
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
-REM exit if not called by a proper script
-IF NOT DEFINED aux_build (
+REM exit if not properly called
+IF NOT DEFINED mode_called (
   EXIT /B)
 
 REM get directory path where this batch resides
 SET "path_this=%~dp0"
 
 REM get path for script that is splitting output
-SET "script_tee=%path_this%out_split.bat"
+SET "script_tee=%path_this%\out_split.bat"
+
+REM - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 REM load build modes
-SET /A build_modes_group_count=0
-FOR /F "usebackq tokens=1* delims=\" %%f IN ("%path_base%build_modes_fpc.txt") DO (
-  SET "fpc_build_modes[!build_modes_group_count!].item=%%~f"
-  SET "fpc_build_modes[!build_modes_group_count!].line=%%g"
+SET /A count_fpc_build_mode_groups=0
+FOR /F "usebackq tokens=1* delims=\" %%f IN ("%path_start%\build_modes_fpc.txt") DO (
+  SET "count_fpc_build_mode_groups[!count_fpc_build_mode_groups!].item=%%~f"
+  SET "count_fpc_build_mode_groups[!count_fpc_build_mode_groups!].line=%%g"
 
-  SET /A build_modes_group_count+=1
+  SET /A count_fpc_build_mode_groups+=1
 )
 
+REM - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
 REM obtain auxiliary paths (path to compilers, path to libraries)
-CALL "%path_this%get_global_paths.bat"
+CALL "%path_this%\get_global_paths.bat"
 
 REM prepare log file name
-SET "file_log=%path_base%log.txt"
+SET "file_log=%path_start%\log.txt"
 
 REM delete log file if it exists
 IF EXIST "%file_log%" (
   DEL "%file_log%")
+  
+REM - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 REM get list of project files and their count
 ECHO Enumerating project files, please wait... | "%script_tee%" "%file_log%"
 ECHO; | "%script_tee%" "%file_log%"
-SET /A file_list_count=0
-SET file_list=
-IF /I "%aux_build%" NEQ "0" (
+SET /A count_projects=0
+SET list_projects=
+IF /I "%mode_aux_build%" NEQ "0" (
   REM compiling auxiliary program (programs used for unit testing)
-  FOR /R "%path_this%..\..\TestBed_Delphi" %%f IN ("*.dpr") DO (
-    SET "file_list=!file_list!,"%%~f""
+  FOR /R "%path_start%..\TestBed_Delphi" %%f IN ("*.dpr") DO (
+    SET "list_projects=!list_projects!,"%%~f""
     ECHO %%~f | "%script_tee%" "%file_log%"
-    SET /A file_list_count+=1
+    SET /A count_projects+=1
   )
-  FOR /R "%path_this%..\..\TestBed_Lazarus" %%f IN ("*.lpi") DO (
-    SET "file_list=!file_list!,"%%~f""
+  FOR /R "%path_start%..\TestBed_Lazarus" %%f IN ("*.lpi") DO (
+    SET "list_projects=!list_projects!,"%%~f""
     ECHO %%~f | "%script_tee%" "%file_log%"
-    SET /A file_list_count+=1
+    SET /A count_projects+=1
   )
 ) ELSE (
   REM building classic program project
-  FOR /R "%path_this%..\..\Dev" %%f IN ("*.dpr","*.lpi") DO (
-    SET "file_list=!file_list!,"%%~f""
+  FOR /R "%path_start%..\Dev" %%f IN ("*.dpr","*.lpi") DO (
+    SET "list_projects=!list_projects!,"%%~f""
     ECHO %%~f | "%script_tee%" "%file_log%"
-    SET /A file_list_count+=1
+    SET /A count_projects+=1
   )
 )
 ECHO; | "%script_tee%" "%file_log%"
-ECHO ...%file_list_count% project file(s) found | "%script_tee%" "%file_log%"
+ECHO ...%count_projects% project file(s) found | "%script_tee%" "%file_log%"
 ECHO; | "%script_tee%" "%file_log%"
+
+REM - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 REM show legend
 CALL "%path_this%functions.bat", :show_legend | "%script_tee%" "%file_log%"
 
 REM prepare command lines for compilation
-SET "cmd_delphi=dcc32 -Q -B"
-SET "cmd_lazarus_old=-B --bm="
-SET "cmd_lazarus=-B --no-write-project --bm="
+SET "var_cmd_delphi=dcc32 -Q -B"
+SET "var_cmd_lazarus_old=-B --bm="
+SET "var_cmd_lazarus=-B --no-write-project --bm="
 
 REM traverse projects and compile them
-SET /A file_list_index=1
-FOR %%f IN (%file_list%) DO (
+SET /A index_projects=1
+FOR %%f IN (%list_projects%) DO (
 
   REM write what project is about to be build
-  ECHO ^[F: !file_list_index!/%file_list_count%^] Building project: | "%script_tee%" "%file_log%"
-  ECHO ^[F: !file_list_index!/%file_list_count%^] %%~f | "%script_tee%" "%file_log%"
+  ECHO ^[F: !index_projects!/%count_projects%^] Building project: | "%script_tee%" "%file_log%"
+  ECHO ^[F: !index_projects!/%count_projects%^] %%~f | "%script_tee%" "%file_log%"
 
   REM Delphi builds
   IF /I "%%~xf"==".dpr" (
     ECHO; | "%script_tee%" "%file_log%"
-    ECHO ^[F: !file_list_index!/%file_list_count%^; C: 1/1^] Delphi | "%script_tee%" "%file_log%"
+    ECHO ^[F: !index_projects!/%count_projects%^; C: 1/1^] Delphi | "%script_tee%" "%file_log%"
 
     CD "%%~dpf"
 
-    %cmd_delphi% "%%~f" | "%script_tee%" "%file_log%"
+    %var_cmd_delphi% "%%~f" | "%script_tee%" "%file_log%"
   )
 
   REM Lazarus builds
@@ -90,47 +98,49 @@ FOR %%f IN (%file_list%) DO (
     CD "%%~dpf"
 
     REM select build modes group
-    SET /A group_index=0
-    CALL :select_build_modes_group "%%~nxf" group_index
-    FOR %%i in (!group_index!) DO (
-      SET "current_modes=!fpc_build_modes[%%i].line!"
+    SET /A var_group_index=0
+    CALL :select_build_modes_group "%%~nxf" var_group_index
+    FOR %%i in (!var_group_index!) DO (
+      SET "list_fpc_build_modes=!count_fpc_build_mode_groups[%%i].line!"
     )
 
     REM get number of build modes
-    SET /A fpc_build_mode_count=0
-    FOR %%a IN (!current_modes!) DO (
-      SET /A fpc_build_mode_count+=1
+    SET /A count_fpc_build_modes=0
+    FOR %%a IN (!list_fpc_build_modes!) DO (
+      SET /A count_fpc_build_modes+=1
     )
 
     REM iterate build modes
-    SET /A fpc_build_mode_index=1
-    FOR %%m in (!current_modes!) DO (
+    SET /A index_fpc_build_modes=1
+    FOR %%m in (!list_fpc_build_modes!) DO (
       REM building in old lazarus
       ECHO; | "%script_tee%" "%file_log%"
-      ECHO ^[F: !file_list_index!/%file_list_count%^; C: !fpc_build_mode_index!/!fpc_build_mode_count!^] Old Lazarus - %%m | "%script_tee%" "%file_log%"
+      ECHO ^[F: !index_projects!/%count_projects%^; C: !index_fpc_build_modes!/!count_fpc_build_modes!^] Old Lazarus - %%m | "%script_tee%" "%file_log%"
 
-      %path_lazb_old% %cmd_lazarus_old%%%m "%%~f" | "%script_tee%" "%file_log%"      
+      %compiler_lazb_old% %var_cmd_lazarus_old%%%m "%%~f" | "%script_tee%" "%file_log%"      
 
       REM building in current lazarus
       ECHO; | "%script_tee%" "%file_log%"
-      ECHO ^[F: !file_list_index!/%file_list_count%^; C: !fpc_build_mode_index!/!fpc_build_mode_count!^] Actual Lazarus - %%m | "%script_tee%" "%file_log%"
+      ECHO ^[F: !index_projects!/%count_projects%^; C: !index_fpc_build_modes!/!count_fpc_build_modes!^] Actual Lazarus - %%m | "%script_tee%" "%file_log%"
 
-      %path_lazb% %cmd_lazarus%%%m "%%~f" | "%script_tee%" "%file_log%"
+      %compiler_lazb% %var_cmd_lazarus%%%m "%%~f" | "%script_tee%" "%file_log%"
       
       REM crossbuilding in lazarus
       ECHO; | "%script_tee%" "%file_log%"
-      ECHO ^[F: !file_list_index!/%file_list_count%^; C: !fpc_build_mode_index!/!fpc_build_mode_count!^] Crosscompiling Lazarus - %%m | "%script_tee%" "%file_log%"
+      ECHO ^[F: !index_projects!/%count_projects%^; C: !index_fpc_build_modes!/!count_fpc_build_modes!^] Crosscompiling Lazarus - %%m | "%script_tee%" "%file_log%"
 
-      %path_lazb_xlin% %cmd_lazarus%%%m "%%~f" | "%script_tee%" "%file_log%"      
+      %compiler_lazb_xlin% %var_cmd_lazarus%%%m "%%~f" | "%script_tee%" "%file_log%"      
 
-      SET /A fpc_build_mode_index+=1
+      SET /A index_fpc_build_modes+=1
     )
   )
 
   ECHO; | "%script_tee%" "%file_log%"
 
-  SET /A file_list_index+=1
+  SET /A index_projects+=1
 )
+
+REM - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 REM wait for user interaction
 ECHO Done | "%script_tee%" "%file_log%"
@@ -138,21 +148,23 @@ ECHO Done | "%script_tee%" "%file_log%"
 
 EXIT /B
 
+REM - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
 REM function selecting proper build group
 REM parameter 1 is name of the file for which the group is being selected
 REM result is stored in second parameter reference
 :select_build_modes_group
   REM find build group for the file
-  SET /A index=%build_modes_group_count%
+  SET /A var_index=%count_fpc_build_mode_groups%
 
   :group_loop
-    SET /A index-=1
-    IF /I "!fpc_build_modes[%index%].item!"=="%~1" (
+    SET /A var_index-=1
+    IF /I "!count_fpc_build_mode_groups[%var_index%].item!"=="%~1" (
       GOTO group_loop_end)
-  IF /I %index% GTR 0 GOTO group_loop
+  IF /I %var_index% GTR 0 GOTO group_loop
   :group_loop_end
 
-  SET "%2=%index%"
+  SET "%2=%var_index%"
 
 EXIT /B
 
